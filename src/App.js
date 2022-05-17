bea604879296985a5442448a6cbbc371d287e149
Roberto Luna <robertocgw@gmail.com>
2022-05-17 13:29:20 -0300

    refatorando de uma nova forma para ver se funciona

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Form from './components/Form';
 import Card from './components/Card';
 
 class App extends React.Component {
-
     state = {
       cardName: '',
       cardDescription: '',
@@ -17,25 +16,25 @@ class App extends React.Component {
       isSaveButtonDisabled: true,
       savedCard: [],
     };
-  }
+}
 
-  onInputChange() {}
+onInputChange = () => {};
 
-  onSaveButtonClick() {}
+onSaveButtonClick = () => {};
 
-  validateFunction() {
-    const attributesMaxLimit = 210;
-    const maxAttribute = 90;
-    const {
-      cardName,
-      cardDescription,
-      cardAttr1,
-      cardAttr2,
-      cardAttr3,
-      cardImage,
-      cardRare,
-    } = this.state;
-    if (cardName !== ''
+validateFunction = () => {
+  const attributesMaxLimit = 210;
+  const maxAttribute = 90;
+  const {
+    cardName,
+    cardDescription,
+    cardAttr1,
+    cardAttr2,
+    cardAttr3,
+    cardImage,
+    cardRare,
+  } = this.state;
+  if (cardName !== ''
       && cardDescription !== ''
       && cardAttr1 !== ''
       && cardAttr2 !== ''
@@ -47,59 +46,12 @@ class App extends React.Component {
       && (Number(cardAttr3) >= 0 && Number(cardAttr3) <= maxAttribute)
       && (Number(cardAttr1)
       + Number(cardAttr2)) + Number(cardAttr3) <= attributesMaxLimit) {
-      this.setState({ isSaveButtonDisabled: false });
-    } else {
-      this.setState({ isSaveButtonDisabled: true });
-    }
+    this.setState({ isSaveButtonDisabled: false });
+  } else {
+    this.setState({ isSaveButtonDisabled: true });
   }
+};
 
-  validateTrunfo() {}
-
-  render() {
-    const {
-      cardName,
-      cardDescription,
-      cardAttr1,
-      cardAttr2,
-      cardAttr3,
-      cardImage,
-      cardRare,
-      cardTrunfo,
-      hasTrunfo,
-      isSaveButtonDisabled,
-    } = this.state;
-    return (
-      <section>
-        <div>
-          <Form
-            cardName={ cardName }
-            cardDescription={ cardDescription }
-            cardAttr1={ cardAttr1 }
-            cardAttr2={ cardAttr2 }
-            cardAttr3={ cardAttr3 }
-            cardImage={ cardImage }
-            cardRare={ cardRare }
-            cardTrunfo={ cardTrunfo }
-            hasTrunfo={ hasTrunfo }
-            isSaveButtonDisabled={ isSaveButtonDisabled }
-            onInputChange={ this.onInputChange }
-            onSaveButtonClick={ this.onSaveButtonClick }
-          />
-        </div>
-        <div>
-          <Card
-            cardName={ cardName }
-            cardDescription={ cardDescription }
-            cardAttr1={ cardAttr1 }
-            cardAttr2={ cardAttr2 }
-            cardAttr3={ cardAttr3 }
-            cardImage={ cardImage }
-            cardRare={ cardRare }
-            cardTrunfo={ cardTrunfo }
-          />
-        </div>
-      </section>
-    );
-  }
+validateTrunfo = () => {};
 
 export default App;
